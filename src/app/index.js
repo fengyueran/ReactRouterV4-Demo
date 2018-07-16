5c69f9e944e1982125f6eb1c22d4dd9564b207b4
xinghunm <xinghunm@curacloudcorp.com>
2018-07-16 16:04:13 +0800

    init repository with create-react-app-xhm cli

---
+++ src/app/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
+import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/configureStore';
+import App from './app';
+
+
+const initialState = {};
+const history = createHistory();
+const store = configureStore(initialState, history);
+
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+  );
+};
+
+export default render;
+registerServiceWorker();
